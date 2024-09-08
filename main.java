bdb857a8c2610b414d1a4207bd71d2b85b7d86c6
lucyimhoff <lucyi4@nycstudents.net>
2024-09-08 23:47:07 +0000

    first commit

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello!");
+        System.out.println();
+        System.out.print("AP");
+        System.out.println();
+        System.out.print("CSA");
+        System.out.println();
+        System.out.print("Goodbye.");
+    }
+}
